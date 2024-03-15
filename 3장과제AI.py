55ba3211ce8a1cc785038b44fff46056b33f672b
dzashaa <dzikris.amaliah@gmail.com>
2024-03-15 20:50:12 +0900

    test 123

---
+++ 3장과제AI.py
@@ -0,0 +1,54 @@
+Python 3.12.2 (tags/v3.12.2:6abddd9, Feb  6 2024, 21:26:36) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+print(100,'+',200,'=', 100+200)
+100 + 200 = 300
+print(200,'+',300,'+',400,'=',200+300+400)
+200 + 300 + 400 = 900
+
+========================== RESTART: C:\김아메\AI\과제\a .py =========================
+10! = 32659200
+
+======================= RESTART: C:\김아메\AI\과제\cel2fah.py =======================
+Celcius	 fahrenheit
+---------------------
+0	32.00
+10	50.00
+20	68.00
+30	86.00
+40	104.00
+50	122.00
+
+======================= RESTART: C:\김아메\AI\과제\fah2cel.py =======================
+화씨 온도를 입력하세요: 86
+섭씨 30.0 도는 화씨 86.0 도 입니다.
+>>> 
+================== RESTART: C:\김아메\AI\과제\circle_calculator.py ==================
+원의 반지름을 입력하세요: 11
+원의 둘래: 69.115024, 원의 면적: 380.132632
+>>> 
+================= RESTART: C:\김아메\AI\과제\pythagorean_theorem.py =================
+민변을 입력하세요: 5
+높이를 입력하세요: 12
+빗변: 13.0
+>>> 
+= RESTART: C:\Users\진한섭\AppData\Local\Programs\Python\Python312\power_of_two.py
+2 4 8 16 32 64 128 256 512 1024 
+>>> 
+= RESTART: C:\Users\진한섭\AppData\Local\Programs\Python\Python312\even_within_range.py
+정수를 입력하세요: 120
+입력된 정수는 0에서 100의 범위 안에 있는 짝수인가요? False
+>>> 
+= RESTART: C:\Users\진한섭\AppData\Local\Programs\Python\Python312\even_within_range.py
+정수를 입력하세요: 88
+입력된 정수는 0에서 100의 범위 안에 있는 짝수인가요? True
+>>> 
+============= RESTART: C:\김아메\AI\과제\binary_and_bitwise_negation.py =============
+정수를 입력하시오: 9
+9의 2진수 값: 0b1001
+9의 2진수 값에 대한 비트단위 부정값: -0b1010
+>>> 
+============= RESTART: C:\김아메\AI\과제\three_digit_number_breakdown.py ============
+세 자리 정수를 입력하세요: 349
+백의 자리: 3
+십의 자리: 4
+일의 자리: 9
